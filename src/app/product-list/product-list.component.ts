187f56c1927bf959069e56ac7788a0ea2cc9b317
HelpDeskMDS <helpdesk@proelectricos.com>
2025-05-19 11:43:39 -0500

    SegundoCambio

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Product } from '../product';
+
+@Component({
+  selector: 'app-product-list',
+  imports: [],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+  products: Product[] = [
+    { id: 1, title: 'Keyboard' },
+    { id: 2, title: 'Microphone' },
+    { id: 3, title: 'Web camera' },
+    { id: 4, title: 'Tablet' }
+  ];
+  
+}
