2c145011beb2dc6b0494c15d9a0c21a7bfc09611
HelpDeskMDS <helpdesk@proelectricos.com>
2025-05-22 16:54:23 -0500

    Padre e Hijos

---
+++ src/app/product-detail/product-detail.component.ts
@@ -0,0 +1,18 @@
+import { Component, input, output } from '@angular/core';
+import { Product } from '../product';
+
+@Component({
+  selector: 'app-product-detail',
+  imports: [],
+  templateUrl: './product-detail.component.html',
+  styleUrl: './product-detail.component.css'
+})
+export class ProductDetailComponent {
+  product = input<Product>();
+  added = output();
+
+  addToCart() {
+    this.added.emit();
+  }
+
+}
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
 import { Product } from '../product';
+import { ProductDetailComponent } from "../product-detail/product-detail.component";
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [ProductDetailComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
+
+  selectedProduct: Product | undefined;
   products: Product[] = [
     { id: 1, title: 'Keyboard' },
     { id: 2, title: 'Microphone' },
     { id: 3, title: 'Web camera' },
-    { id: 4, title: 'Tablet' }
+    { id: 4, title: 'PS5' }
   ];
+  
+  onAdded() {
+    alert(`${this.selectedProduct?.title} added to the cart!`);
+  }
   
 }
